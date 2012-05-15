f729db41f411da0c269e92dfa16401e086b0f649
ABE, Tatsuki <ABE.Tatsuki@gmail.com>
2012-05-16 03:08:06 +0900

    spork の動作を改善

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -43,7 +43,11 @@ end
 
 Spork.each_run do
   # This code will be run each time you run your specs.
-
+  silence_warnings do
+    Dir[Rails.root.join('app/**/*.rb')].each do |file|
+      load file
+    end
+  end
 end
 
 # --- Instructions ---
