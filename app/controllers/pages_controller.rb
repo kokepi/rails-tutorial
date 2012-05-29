f12c6cea63f01ffc4fdb83b0debb88c2fb14b60e
ABE, Tatsuki <ABE.Tatsuki@gmail.com>
2012-05-29 12:09:52 +0900

    done with the static pages

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,12 +1,18 @@
 class PagesController < ApplicationController
 
   def home
+    @title = 'Home'
+    @desc = 'This is the home page for sample app'
   end
 
   def contact
+    @title = 'Contact'
+    @desc = 'This is the contact page for sample app'
   end
 
   def about
+    @title = 'About'
+    @desc = 'This is the about page for sample app'
   end
 
 end
