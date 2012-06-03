c18f12b9bd15e9f6dc9f8a8eda5eaa56d4b00683
ABE, Tatsuki <ABE.Tatsuki@gmail.com>
2012-06-03 15:06:28 +0900

    login function and tests. sessions helper.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,21 @@
 class SessionsController < ApplicationController
+
   def new
+    @title = 'Sign in'
+  end
+  def create
+    @user = User.authenticate(params[:session][:email], params[:session][:password])
+    if @user.nil?
+      flash.now[:error] = "Email and Password don't match."
+      render 'new'
+    else
+      sign_in @user 
+      flash[:success] = "Thank you for visit again."
+      redirect_to @user
+    end
+    
+  end
+  def destory
+    
   end
 end
--- spec/controllers/sessions_controller_spec.rb
+++ spec/controllers/sessions_controller_spec.rb
@@ -1,6 +1,7 @@
 require 'spec_helper'
 
 describe SessionsController do
+  render_views
 
   describe "GET 'new'" do
     it "returns http success" do
@@ -9,4 +10,50 @@ describe SessionsController do
     end
   end
 
+  describe 'POST "create"' do
+
+    describe 'widh invalid email and password' do
+
+      before(:each) do
+        @attr = { :email => "email@example.com", :password => "invalid" }
+      end
+
+      it "should re-render the new page" do
+        post :create, :session => @attr
+        response.should_not render_template('user/show')
+        response.should render_template('new')
+      end
+
+      it "should have a flash-error message" do
+        post :create, :session => @attr
+        response.should have_selector("div.flash.alert-error")
+      end
+
+    end
+
+    describe 'with valid email and password' do
+
+      before(:each) do
+        @user = Factory(:user)
+        @attr = { :email => @user.email, :password => @user.password }
+      end
+
+      it "should be current_user == @user" do
+        post :create, :session => @attr
+        controller.current_user.should == @user
+        controller.signed_in?.should be_true
+      end
+
+      it "should redirect_to the user show page" do
+        post :create, :session => @attr
+        response.should redirect_to(user_path(@user))
+      end
+
+      it "should have a flash-success message" do
+        post :create, :session => @attr
+        flash[:success].should_not be_nil
+      end
+
+    end
+  end
 end
