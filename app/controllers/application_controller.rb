1862026e025964619ed464c8d632512f86aab468
ABE, Tatsuki <ABE.Tatsuki@gmail.com>
2012-06-15 01:00:58 +0900

    friendly forwarding

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -27,7 +27,13 @@ class ApplicationController < ActionController::Base
   end
 
   def deny_access
+    store_location
     redirect_to signin_path, :notice => "Please sign in to access this page."
+  end
+
+  def redirect_back_or(default)
+    redirect_to(session[:return_to] || default)
+    clear_return_to
   end
 
   private
@@ -38,6 +44,13 @@ class ApplicationController < ActionController::Base
 
   def remember_token
     cookies.signed[:remember_token] || [ nil,nil ]
+  end
+
+  def store_location
+    session[:return_to] = request.fullpath
+  end
+  def clear_return_to
+    session[:return_to] = nil
   end
 
 end
