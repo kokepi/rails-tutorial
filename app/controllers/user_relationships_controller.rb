f74a5425e1ae702b09ed3ff26ac6b0049e0359dc
kokepi <kokepiking+github@gmail.com>
2012-07-07 15:32:51 +0900

    ajax follow button proto

--- app/controllers/user_relationships_controller.rb
+++ app/controllers/user_relationships_controller.rb
@@ -4,12 +4,18 @@ class UserRelationshipsController < ApplicationController
   def create
     @user = User.find(params[:user_relationship][:followed_id])
     current_user.follow!(@user)
-    redirect_to @user
+    respond_to do |format|
+      format.html{ redirect_to @user }
+      format.js
+    end
   end
 
   def destroy
     @user = UserRelationship.find(params[:id]).followed
     current_user.unfollow!(@user)
-    redirect_to @user
+    respond_to do |format|
+      format.html{ redirect_to @user }
+      format.js
+    end
   end
 end
